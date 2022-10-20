df10f6752b797ae70b61f54bf6902dcd690a5204
laughingbiscuit <sean@laughingbiscuit.com>
2022-10-20 14:25:21 +0000

    gh actions not a tty

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 set -e
 
 docker build -t blah .
-docker run --net=host -v $(pwd)/results:/root/myproject/target/karate-reports/  -v /var/run/docker.sock:/var/run/docker.sock -it blah 
+docker run --net=host -v $(pwd)/results:/root/myproject/target/karate-reports/  -v /var/run/docker.sock:/var/run/docker.sock -i blah 
