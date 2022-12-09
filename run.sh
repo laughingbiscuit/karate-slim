19987123a44be8f9b164242c5d3629176af9106c
laughingbiscuit <sean@laughingbiscuit.com>
2022-12-09 12:49:16 +0000

    tweak docker run cmd

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 set -e
 
-docker build -t blah .
-docker run --privileged --net=host -v $(pwd)/results:/root/myproject/target/karate-reports/ -v /var/run/docker.sock:/var/run/docker.sock -i blah 
+docker build -t karate-chrome .
+docker run --name karate --rm --cap-add=SYS_ADMIN -v "$PWD":/karate -v "$HOME"/.m2:/root/.m2 karate-chrome &
+sleep 5
+docker exec karate mvn test -B -ntp
+docker stop karate
+wait
