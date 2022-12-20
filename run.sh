0c9661f089d8e5df02131dce71452571ed9186a3
laughingbiscuit <sean@laughingbiscuit.com>
2022-12-20 11:04:19 +0000

    gh actions isnt a tty

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ set -e
 docker build -t blah .
 docker run -d --name karate --rm -p 5900:5900 --cap-add=SYS_ADMIN -v $(pwd)/results:/karate/target/karate-reports blah
 sleep 2
-docker exec -it karate mvn test
+docker exec -i karate mvn test
 docker stop karate
