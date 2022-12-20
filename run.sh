3f48b25d326cde9720140c3d3b23cafe0de8f190
laughingbiscuit <sean@laughingbiscuit.com>
2022-12-20 11:10:31 +0000

    uplaod entire target dir

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-docker build -t blah .
-docker run -d --name karate --rm -p 5900:5900 --cap-add=SYS_ADMIN -v $(pwd)/results:/karate/target/karate-reports blah
+docker build -t karate-slim .
+docker run -d --name karate --rm -p 5900:5900 --cap-add=SYS_ADMIN -v $(pwd)/results:/karate/target karate-slim
 sleep 2
 docker exec -i karate mvn test
 docker stop karate
