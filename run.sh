4fa4a58505321f2e79d526374e6a7263baa5e85c
laughingbiscuit <sean@laughingbiscuit.com>
2023-02-12 08:41:17 +0000

    debug

--- run.sh
+++ run.sh
@@ -5,11 +5,12 @@ set -e
 
 (cd features && for FILE in *.hfeature; do
   sh ../helpers/humanify.sh > $(echo $FILE | sed 's/hfeature/feature/')
+  cat $(echo $FILE | sed 's/hfeature$/feature/')
 done)
 
 # run tests
 docker build -t karate-slim .
 docker run -d --name karate --rm -p 5900:5900 --cap-add=SYS_ADMIN -v $(pwd)/results:/karate/target karate-slim
 sleep 2
-docker exec -i karate mvn test
+docker exec -i karate mvn test -B
 docker stop karate
