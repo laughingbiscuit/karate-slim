d7737b5cbbd462e0f88ba8f99289634928c2ff47
laughingbiscuit <sean@laughingbiscuit.com>
2023-02-12 08:49:53 +0000

    quote url

--- run.sh
+++ run.sh
@@ -11,5 +11,5 @@ done)
 docker build -t karate-slim .
 docker run -d --name karate --rm -p 5900:5900 --cap-add=SYS_ADMIN -v $(pwd)/results:/karate/target karate-slim
 sleep 2
-docker exec -i karate mvn test -B
+docker exec -i karate mvn test -q
 docker stop karate
