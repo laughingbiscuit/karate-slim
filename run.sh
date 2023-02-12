7f647c0824764b5e54a20d16262f6315ca57d14f
laughingbiscuit <sean@laughingbiscuit.com>
2023-02-12 08:46:28 +0000

    fix

--- run.sh
+++ run.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
-set -e
+set -ex
 
 # preprocess - humanify all
 
 (cd features && for FILE in *.hfeature; do
-  sh ../helpers/humanify.sh > $(echo $FILE | sed 's/hfeature/feature/')
+  sh ../helpers/humanify.sh $FILE > $(echo $FILE | sed 's/hfeature/feature/')
   cat $(echo $FILE | sed 's/hfeature$/feature/')
 done)
-
 # run tests
 docker build -t karate-slim .
 docker run -d --name karate --rm -p 5900:5900 --cap-add=SYS_ADMIN -v $(pwd)/results:/karate/target karate-slim
