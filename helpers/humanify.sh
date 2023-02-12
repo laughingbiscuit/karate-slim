7ce8190bf7c5d1e59fea8866135afdfaf71cb615
laughingbiscuit <sean@laughingbiscuit.com>
2023-02-12 09:17:59 +0000

    try getting html

--- helpers/humanify.sh
+++ helpers/humanify.sh
@@ -7,5 +7,5 @@ s|I wait|delay(2000)|
 s|I go back|back()|
 s|I click \(.*\)|click('\1')|
 s|I type \(.*\) into \(.*\)|input('input[name=\2]', '\1')|
-s|the page says \(.*\)|match value('*') contains '\1'|
+s|the page says \(.*\)|match html('.html') contains '\1'|
 " "$@" 
